47c1522e910cc41ff902cb4a508d6b0913301733
sbboard <colin.buffum@gmail.com>
2019-10-27 14:21:58 -0600

    trimmed the fat

---
+++ simpleRead.py
@@ -0,0 +1,23 @@
+from py532lib.i2c import *
+from py532lib.frame import *
+from py532lib.constants import *
+import time
+import os
+from threading import Thread
+import cv2
+import numpy as np
+
+#listen for nfc cards    
+def read_card():
+    while True:
+        pn532 = Pn532_i2c()
+        pn532.SAMconfigure()
+
+        card_data = pn532.read_mifare().get_data()
+
+
+        print(card_data)
+
+
+if __name__ == '__main__':
+    Thread(target = read_card).start()
