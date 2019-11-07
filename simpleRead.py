120f17874e6c84e7e6652f1f362c882b860208b2
sbboard <colin.buffum@gmail.com>
2019-11-06 22:55:32 -0700

    took out unneeded comments / cv2 import

--- simpleRead.py
+++ simpleRead.py
@@ -5,11 +5,9 @@ from py532lib.mifare import *
 import time
 import os
 from threading import Thread
-import cv2
 import numpy as np
 import ndef
-    
-#listen for nfc cards    
+
 def read_card():
     while True:
         card = Mifare()
@@ -17,18 +15,15 @@ def read_card():
         card.set_max_retries(MIFARE_SAFE_RETRIES)
         uid = card.scan_field()
         if uid:
-            #print([hex(i) for i in uid]) #card uid
             blocksToExplore = [0,1,2,3]
             dataArray = []
             for block in blocksToExplore:
-                address = card.mifare_address(1,block) #read sector 1 block zero of card
+                address = card.mifare_address(1,block)
                 card.mifare_auth_b(address,MIFARE_FACTORY_KEY)
                 data = card.mifare_read(address)
-                card.in_deselect() # In case you want to authorize a different sector.
-
-                #print(data) #data in bytearray form
+                card.in_deselect()
                 dataArray.extend([hex(x) for x in data]) #data in array form
-            #print(dataArray)
+
             fiftyFourlocation = dataArray.index('0x6e')+1
             felocation = dataArray.index('0xfe')
             dataArray = dataArray[fiftyFourlocation:felocation]
@@ -42,9 +37,6 @@ def read_card():
             s = bytes.fromhex(s).decode('ascii')
             print(s)
             time.sleep(5)
-            
-            #end of new stuff
-
 
 if __name__ == '__main__':
     #Thread(target = open_img).start()
